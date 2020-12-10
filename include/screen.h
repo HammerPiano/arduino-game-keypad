33a65fc7938a5cdd5b8907fae3e7221e42e36f90
HammerPiano <noamw2000@gmail.com>
2020-12-10 13:36:12 +0200

    Moved and optimized the display code

---
+++ include/screen.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeMono9pt7b.h>
+
+#define SCREEN_WIDTH (128)
+#define SCREEN_HEIGHT (64)
+
+/*
+ ? My version of SSD1306 screen is divided to 2 parts, top quarter has yellow pixels, and the rest has cyan pixels
+ ? These definitions are the Y start and end of each section, since they are dividing the rows
+ ? Yellow section is used for titles, texts...	- 128x16
+ ? Cyan section is for pictures					- 128x48
+*/
+#define SCREEN_YELLOW_START (0)
+#define SCREEN_YELLOW_END (15)
+#define SCREEN_BLUE_START (16)
+#define SCREEN_BLUE_END (63)
+
+#define SCREEN_BLUE_HEIGHT (SCREEN_HEIGHT - SCREEN_BLUE_START)
+
+// ? floor((SCREEN_WIDTH) / character width of the font = 11
+// ? A good aproximation for the mono-space font
+#define SCREEN_TITLE_LENGTH (11)
+#define SCREEN_TITLE_X_OFFSET (0)
+#define SCREEN_TITLE_Y_OFFSET (12)
+// For somereason, my address select in the module is soldered incorrectly
+#define SCREEN_ADDRESS (0x3C)
+
+
+class Screen
+{
+public:
+	Screen() : _display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire),
+															_title(""), _bit_map(nullptr)
+	{
+		
+	}
+	
+	void begin(const String & title, const uint8_t * bit_map)
+	{
+		
+		_display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+		_display.display();
+		delay(2000);// Give Adafruit some time on screen for their lovely job
+		_display.setFont(&FreeMono9pt7b);
+		_display.setTextColor(WHITE);
+		// Reset the display
+		_display.clearDisplay();
+		// draw the underline
+		_display.drawLine(0, SCREEN_YELLOW_END, SCREEN_WIDTH - 1, SCREEN_YELLOW_END, WHITE);
+		set_title(title);
+		set_bit_map(bit_map);
+	}
+
+	/**
+	 * @brief Set the title string
+	 * 
+	 * 
+	 * @param title 
+	 * 
+	 * @remark In order to print the whole title on the screen, the length must be 11 characters or less
+	 * @remark If len(title)>11, call scroll_title to manually scroll the title to show all characters
+	 */
+	void set_title(const String & title)
+	{
+		_current_index = 0;
+		this->_title = title;
+		cyclic_title_print(SCREEN_TITLE_LENGTH);
+	}
+
+
+
+	/**
+	 * @brief Scrolls the title
+	 * 
+	 * @remark if the title is 11 characters or less, this function has no effect 
+	 */
+	void scroll_title()
+	{
+		_current_index++;
+		if (_current_index > _title.length())
+		{
+			_current_index = 0;
+		}
+		cyclic_title_print(SCREEN_TITLE_LENGTH, _current_index);
+	}
+
+	void set_bit_map(const uint8_t * bit_map)
+	{	
+		// Clear the icon section
+		this->_bit_map = bit_map;
+		_display.fillRect(0, SCREEN_BLUE_START, SCREEN_WIDTH, SCREEN_BLUE_HEIGHT, BLACK);
+		if (bit_map == nullptr)
+		{
+			return;
+		}
+		_display.drawBitmap(0, SCREEN_BLUE_START, bit_map, SCREEN_WIDTH, SCREEN_BLUE_HEIGHT, WHITE);
+		_display.display();
+	}
+
+private:
+
+	void cyclic_title_print(size_t char_num, size_t start_index=0)
+	{
+		if (char_num > this->_title.length())
+		{
+			char_num = this->_title.length();
+		}
+		// Clear the title (execpt the underline)
+		this->_display.fillRect(0, 0, SCREEN_WIDTH, SCREEN_YELLOW_END, BLACK);
+		this->_display.setCursor(SCREEN_TITLE_X_OFFSET, SCREEN_TITLE_Y_OFFSET);
+		for (size_t i = 0; i < char_num; i++)
+		{
+			/* Cyclic print of the title */
+			_display.print(_title[(start_index + i) % this->_title.length()]);
+		}
+		_display.display();
+	}
+	size_t _current_index = 0;
+	Adafruit_SSD1306 _display;
+	String _title;
+	const uint8_t * _bit_map;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,17 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Fonts/FreeMono9pt7b.h>
-#include <Adafruit_SSD1306.h>
-#include "bmps/elite_logo.h"
-#define SCREEN_BLUE_SECTION (16)
-#define SCREEN_YELLOW_SECTION (48)
-#define SCREEN_WIDTH (128) // OLED display width, in pixels
-#define SCREEN_HEIGHT (SCREEN_BLUE_SECTION + SCREEN_YELLOW_SECTION) // OLED display height, in pixels
+#include <screen.h>
+#include <bmps/elite_logo.h>
 
-// For somereason, my address select in the module is soldered incorrectly
-#define DISPLAY_ADDRESS (0x3C)
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
-
-void test_print(uint8_t offset)
-{
-	display.clearDisplay();
-	display.setFont(&FreeMono9pt7b);
-	display.setTextColor(SSD1306_WHITE); // Draw white text
-	display.setCursor(0, offset);     // Start at top-left corner
-	display.print("Lorem Ipsum");
-	display.drawLine(0, 15, 126, 15, WHITE);// "underline"
-	display.display();
-}
+Screen screen;
 
 void setup()
 {
-	if(!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS))
-	{ // Address 0x3D for 128x64
-		for(;;); // Don't proceed, loop forever
-	}
-	test_print(12);
-	display.drawBitmap(0, 16, BMP_ELITE_LOGO, ELITE_LOGO_WIDTH, ELITE_LOGO_HEIGHT, WHITE);
-	display.display();
+	/* The space after dangrous is to make the scrolling look better */
+	screen.begin("Elite Dangerous ", BMP_ELITE_LOGO);
 }
 
 void loop()
 {
-
+	screen.scroll_title();
+	delay(200);
 }
