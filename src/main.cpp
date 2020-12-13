7c3950e255e849a4bd4d517402fb904332581651
HammerPiano <noamw2000@gmail.com>
2020-12-13 15:07:27 +0200

    Added test main file

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,9 @@
 
 const uint8_t KEYPAD_ROWS[] = {4,5,6,7};
 const uint8_t KEYPAD_COLS[] = {8,9,10};
-char keys[KEYPAD_ROW_COUNT][KEYPAD_COL_COUNT] = {
-{'1','2','3'},
-{'4','5','6'},
-{'7','8','9'},
-{'*','0','#'}
-};
+String keys[] = {"Lorem", "Ipsum", "Test", "Foo", "Bar", "Baz", "Ahhh", "Arduino", "STM32", "One", "Two", "Three"};
 
-Keypad kpd = Keypad( makeKeymap(keys), KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ROW_COUNT, KEYPAD_COL_COUNT);
+auto kpd = Keypad<String>( keys, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ROW_COUNT, KEYPAD_COL_COUNT);
 
 Screen screen;
 uint32_t time_stamp = 0;
@@ -31,17 +26,7 @@ void loop()
 {	
 	if (kpd.getKeys())
     {
-        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
-        {
-            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
-            {
-                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
-                    case PRESSED:
-					screen.set_title(String(kpd.key[i].kchar));
-                break;
-                }
-            }
-        }
+		screen.set_title(kpd[kpd.get_pressed_pins()[0]]);
     }
 	if ((millis() - time_stamp) > 200)
 	{
