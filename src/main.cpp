cd23c5f6fd93e992630e5c3c75a45c32fda3dafe
HammerPiano <noamw2000@gmail.com>
2021-10-09 12:55:38 +0300

    Made control+command buttons combinations

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 
 #define NO_BUTTON (0xFF)
 
+#define CONTROL_BUTTON_MINIMUM ('A')
+#define COMMAND_BUTTON_MINIMUM ('1')
+
 const uint8_t KEYPAD_ROWS[] = {4,5,6,7};
 const uint8_t KEYPAD_COLS[] = {8,9,10};
 char keys[] = {'A', 'B', 'C',
@@ -24,32 +27,50 @@ char keys[] = {'A', 'B', 'C',
 
 Keypad kpd = Keypad( keys, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ROW_COUNT, KEYPAD_COL_COUNT);
 // Oh god I hate this soo much, but I need only buttons not a whole plane
-Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK, 9, 0, false, false, false, false, false, false, false, false, false, false, false);
+Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK, 12, 0, false, false, false, false, false, false, false, false, false, false, false);
 Screen screen;
 uint32_t time_stamp = 0, button_press_time = 0;
-byte pressed_button = NO_BUTTON;
+byte current_command_button = NO_BUTTON;
+byte current_control_button = 0;
 
 void setup()
 {
 	/* The space after dangrous is to make the scrolling look better */
 	//screen.begin("Elite Dangerous", ELITE_DANGEROUS_BMP);
+
+	// Init control buttons
+	current_control_button = 0;
+
+	// Start USB sub-process
 	joystick.begin();
 }
 
 void loop()
 {	
-	if (kpd.check_keypad() && pressed_button == NO_BUTTON)
+	if (kpd.check_keypad() && current_command_button == NO_BUTTON)
     {
-		byte pin = kpd.get_pressed_pins()[0];
-		byte pin_value = kpd[pin] - '1';
-		//screen.set_title(String((kpd[pin])));
-		if (pin_value <= 9)
+		byte* pressed_buttons = kpd.get_pressed_pins();
+		byte count = kpd.get_pressed_pins_count();
+		for (byte i = 0; i < count; i++)
 		{
-			joystick.pressButton(pin_value);
-			pressed_button = pin_value;
-			button_press_time = millis();
+			if (kpd[pressed_buttons[i]] >= CONTROL_BUTTON_MINIMUM)
+			{
+				current_control_button = pressed_buttons[i];
+			}
+			else
+			{
+				current_command_button = pressed_buttons[i];
+			}
 		}
-
+		//screen.set_title(String((kpd[pin])));
+		if (current_command_button != NO_BUTTON)
+		{
+			button_press_time = millis();
+			joystick.pressButton(current_control_button);
+			delay(20);
+			joystick.pressButton(current_command_button);
+		}
+		
     }
 	if ((millis() - time_stamp) > 200)
 	{
@@ -57,9 +78,10 @@ void loop()
 		//screen.scroll_title();
 	}
 
-	if (pressed_button != NO_BUTTON && ((millis() - button_press_time) >= BUTTON_RELEASE_INTERVAL))
+	if (current_command_button != NO_BUTTON && ((millis() - button_press_time) >= BUTTON_RELEASE_INTERVAL))
 	{
-		joystick.releaseButton(pressed_button);
-		pressed_button = NO_BUTTON;
+		joystick.releaseButton(current_command_button);
+		joystick.releaseButton(current_control_button);
+		current_command_button = NO_BUTTON;
 	}
 }
